398c3c7e79008e924867a90d07d53e4ca1fef654
rayd56 <ebelerayd@gmail.com>
2026-01-11 18:10:51 +0100

    index.js

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
 function startProject() {
-    const child = spawn("node", ["Sakura.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true,
-        env: { ...process.env, PORT: process.env.PORT || 1000 } // Port 1000 default
-    });
+    const child = spawn(
+        "node",
+        ["Sakura.js"],
+        {
+            cwd: __dirname,
+            stdio: "inherit", // IMPORTANT pour Render
+            shell: true,
+            env: {
+                ...process.env,
+                PORT: process.env.PORT || 1000
+            }
+        }
+    );
 
     child.on("close", (code) => {
-        if (code == 2) {
+        log.info(`Sakura.js stopped with code ${code}`);
+
+        if (code === 2) {
             log.info("Restarting Project...");
             startProject();
         }
